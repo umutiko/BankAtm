2af97e16966ba87ad6f019cb82eee201c8d501d3
umutiko <umutcesur5959@gmail.com>
2020-05-25 17:10:08 +0300

    sadadasd

--- BankAtm/Program.cs
+++ BankAtm/Program.cs
@@ -8,11 +8,17 @@ namespace BankAtm
 {
     class Program
     {
+        int[] accounts = { 2500, 1200, 6000, 10000, 7450, 4569 };
+        static string repeat;
+        static int processNumber;
+        int accountNumber;
+        int amount;
+
         static void Main(string[] args)
         {
             void ShowMenu()
             {
-                
+
                 Console.WriteLine("Brave ATM Welcome :)");
                 Console.WriteLine("---------------------");
                 Console.WriteLine("Please select your transaction");
@@ -22,10 +28,8 @@ namespace BankAtm
 
             Console.WriteLine("Press a key to get started");
             Console.ReadLine();
-            
-            int[] accounts = { 2500, 1200, 6000, 10000, 7450, 4569 };
-            string repeat;
-            int processNumber;
+
+
             do
             {
                 ShowMenu();
@@ -36,79 +40,7 @@ namespace BankAtm
             }
             while (repeat == "y" || repeat == "Y");
 
-            void Process()
-            {
-                
-                Console.WriteLine("Choose an account  :");
-                ShowAccounts();
 
-                int accountNumber = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("Enter the amount");
-                int amount = Convert.ToInt32(Console.ReadLine());
-
-                switch (processNumber)
-                {
-                    case 1:
-                        MakeADeposit(accountNumber, amount, ProcessType.WithdrawMoney);
-                        break;
-                    case 2:
-                        MakeADeposit(accountNumber, amount, ProcessType.DepositMoney);
-                        break;
-
-                }
-
-            }
-            void MakeADeposit(int accountNumber, int amount, ProcessType processType)
-            {
-                for (int i = 1; i <= accounts.Length; i++)
-                {
-                    if (i == accountNumber)
-                    {
-                        int account = accounts[i - 1];
-                        account = processType == ProcessType.WithdrawMoney ? account - amount : processType == ProcessType.DepositMoney ? account + amount : account;
-
-                        Console.WriteLine("Your transaction is in progress,please wait...");
-                        System.Threading.Thread.Sleep(1500);
-                        if (account >= 0)
-                        {
-                            accounts[i - 1] = account;
-                            ShowAccounts(accountNumber);
-                        }
-                        else
-                        {
-                            Console.WriteLine("You don't have enough funds !");
-                            ShowAccounts(accountNumber);
-                        }
-
-                    }
-                }
-
-            }
-
-            void ShowAccounts(int index = 0)
-            {
-                if (index > 0)
-                {
-                    Console.WriteLine("Account");
-                    Console.WriteLine("--------------");
-                    for (int i = 1; i <= accounts.Length; i++)
-                    {
-                        if (index == i)
-                            Console.WriteLine($"Account Number: {i} | Available Balance: {accounts[i - 1]}");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Accounts");
-                    Console.WriteLine("--------------");
-                    for (int i = 1; i <= accounts.Length; i++)
-                    {
-                        Console.WriteLine($"Account Number: {i} | Available Balance: {accounts[i - 1]}");
-                    }
-                }
-
-            }
 
         }
         enum ProcessType
@@ -116,5 +48,80 @@ namespace BankAtm
             WithdrawMoney,
             DepositMoney
         }
+        void ShowAccounts(int index = 0)
+        {
+            if (index > 0)
+            {
+                Console.WriteLine("Account");
+                Console.WriteLine("--------------");
+                for (int i = 1; i <= accounts.Length; i++)
+                {
+                    if (index == i)
+                        Console.WriteLine($"Account Number: {i} | Available Balance: {accounts[i - 1]}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Accounts");
+                Console.WriteLine("--------------");
+                for (int i = 1; i <= accounts.Length; i++)
+                {
+                    Console.WriteLine($"Account Number: {i} | Available Balance: {accounts[i - 1]}");
+                }
+            }
+
+        }
+        void MakeADeposit(int accountNumber, int amount, ProcessType processType)
+        {
+            for (int i = 1; i <= accounts.Length; i++)
+            {
+                if (i == accountNumber)
+                {
+                    int account = accounts[i - 1];
+                    account = processType == ProcessType.WithdrawMoney ? account - amount : processType == ProcessType.DepositMoney ? account + amount : account;
+
+                    Console.WriteLine("Your transaction is in progress,please wait...");
+                    System.Threading.Thread.Sleep(1500);
+                    if (account >= 0)
+                    {
+                        accounts[i - 1] = account;
+                        ShowAccounts(accountNumber);
+                    }
+                    else
+                    {
+                        Console.WriteLine("You don't have enough funds !");
+                        ShowAccounts(accountNumber);
+                    }
+
+                }
+            }
+
+        }
+        static void Process()
+        {
+
+            Console.WriteLine("Choose an account  :");
+            ShowAccounts();
+
+            accountNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the amount");
+            amount = Convert.ToInt32(Console.ReadLine());
+
+            switch (processNumber)
+            {
+                case 1:
+                    MakeADeposit(accountNumber, amount, ProcessType.WithdrawMoney);
+                    break;
+                case 2:
+                    MakeADeposit(accountNumber, amount, ProcessType.DepositMoney);
+                    break;
+
+            }
+
+        }
+
+
+
     }
 }
